977af6c61a1954dfec24b914bc1052a32c23a799
trongdth <trongdth@gmail.com>
2019-06-21 18:47:39 +0700

    Fix UI

--- lib/screens/import.dart
+++ lib/screens/import.dart
@@ -3,6 +3,7 @@ import 'package:blockpass/services/user.dart';
 import 'package:blockpass/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:blockpass/widgets/combobox_widget.dart';
+import 'package:flutter/widgets.dart';
 
 class ImportScreen extends StatefulWidget {
 
@@ -32,6 +33,10 @@ class _ImportScreenState extends State<ImportScreen> {
 
   }
 
+  void createEOSAccount() async {
+    Utils.showPopup(context, 'INFO', 'Comming soon!');
+  }
+
   @override
   void dispose() {
     privController.dispose();
@@ -52,137 +57,125 @@ class _ImportScreenState extends State<ImportScreen> {
         leading: Container(),
       ),
       body: Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 8,
-              child: Container(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 2,
-                      child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Container(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                          child: Text(
-                            'Your private key',
-                            style: Theme.of(context).textTheme.subtitle,
-                          ),
-                        ),
-                      )
-                    ),
-                    Expanded(
-                      flex: 1, 
-                      child: Container(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              height: 60,
-                              color: Colors.white,
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child:
-                                Container(
-                                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: TextField(
-                                    controller: privController,
-                                    decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: '',
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ),
-                          ],
-                        ),
+        child:
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Your private key',
+                        style: Theme.of(context).textTheme.subtitle,
                       ),
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                          children: <Widget>[
+                  ),
+                  
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 60,
+                          color: Colors.white,
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child:
                             Container(
-                              height: 60,
-                              child: ComboboxWidget(
-                                lblLeading: 'EOS',
-                                lblContent: _selectedChain,
-                                entries: app.eosChains,
-                                onChangedChain: (name) => {
-                                  _selectedChain = name
-                                },),
-                            )
-                          ],
-                        ), 
-                    ),
-                    Expanded(
-                      flex: 1, 
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: 60,
-                            child: RaisedButton(
-                              color: Color.fromRGBO(36, 59, 107, 1),
-                              onPressed: () => btnNextClicked(),
-                              child: Text(
-                                'Next',
-                                style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 16),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: Column(
-                        children: <Widget>[
-                          Container(  
-                            height: 40,
-                            child: FlatButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  'Already have an account? Login here',
-                                  style: TextStyle(
-                                    decoration: TextDecoration.underline,
-                                    color: Theme.of(context).primaryColor,
-                                  ),
+                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                              child: TextField(
+                                controller: privController,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: '',
                                 ),
                               ),
                             ),
                           )
-                        ],
-                      )
+                        ),
+                      ],
                     ),
-                    // Expanded(
-                    //   flex: 1,
-                    //   child: FlatButton(
-                    //     onPressed: () => print('Skip touched'),
-                    //     child: Align(
-                    //       alignment: Alignment.bottomCenter,
-                    //       child: Text(
-                    //         'Skip',
-                    //         style: TextStyle(
-                    //           decoration: TextDecoration.underline,
-                    //           color: Theme.of(context).primaryColor,
-                    //         ),
-                    //       ),
-                    //     ),
-                    //   ),
-                    // ),
-                  ],
-                ),
+                  ),
+                  
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    child: Container(
+                      height: 60,
+                      child: ComboboxWidget(
+                        lblLeading: 'EOS',
+                        lblContent: _selectedChain,
+                        entries: app.eosChains,
+                        onChangedChain: (name) => {
+                          _selectedChain = name
+                        },),
+                    ),
+                  ),
+                    
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      child: RaisedButton(
+                        color: Color.fromRGBO(36, 59, 107, 1),
+                        onPressed: () => btnNextClicked(),
+                        child: Text(
+                          'Next',
+                          style: TextStyle(color: Color(0xFFFFFFFF), fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Column(
+                    children: <Widget>[
+                      Container(  
+                        height: 40,
+                        child: FlatButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Already have an account? Login here',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    child: Text(
+                      '-- Or --',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColorLight,
+                      ),
+                    ),
+                  ),
+                  
+                  FlatButton(
+                    onPressed: () => createEOSAccount(),
+                    child: Text(
+                      'Create a free EOS account on Kylin Testnet',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      )
+          ),
+      ),
     );
   }
 }
